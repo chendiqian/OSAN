0159c15727d76b3bacc6f4aa9e9717a41795d54d
Chendi Qian <1049653012@qq.com>
2022-01-27 15:25:20 +0100

    add eval function

--- train.py
+++ train.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, List
 import itertools
 
 import torch
@@ -8,7 +8,7 @@ from torch_geometric.data import Batch
 
 from models import NetGINE, NetGCN
 from custom_dataloder import MYDataLoader
-from sample_subgraph import subgraphs_from_index
+from sample_subgraph import subgraphs_from_index, subgraphs_from_mask
 
 from imle.wrapper import imle
 from imle.target import TargetDistribution
@@ -19,6 +19,32 @@ Optimizer = Union[torch.optim.Adam,
 Emb_model = Union[NetGCN]
 Train_model = Union[NetGINE]
 Loss = Union[torch.nn.modules.loss.MSELoss, torch.nn.modules.loss.L1Loss]
+
+
+def make_get_batch_topk(ptr, sample_k):
+    @torch.no_grad()
+    def torch_get_batch_topk(logits: torch.Tensor) -> torch.Tensor:
+        """
+        Select topk in each col, return as float of 0 and 1s
+
+        :param logits:
+        :return:
+        """
+        logits = logits.detach()
+        logits = torch.tensor_split(logits, ptr)
+
+        sample_node_idx = []
+        for l in logits:
+            thresh = torch.topk(l, k=min(sample_k, l.shape[0]), dim=0, sorted=True).values[-1, :]   # kth largest
+            # shape (n_nodes, dim)
+            mask = (l >= thresh[None]).to(torch.float)
+            sample_node_idx.append(mask)
+
+        sample_node_idx = torch.cat(sample_node_idx, dim=0)
+        sample_node_idx.requires_grad = False
+        return sample_node_idx
+
+    return torch_get_batch_topk
 
 
 def train(sample_k: int,
@@ -64,15 +90,15 @@ def train(sample_k: int,
             return torch_get_batch_topk(logits)
 
         sample_node_idx = imle_get_batch_topk(logits)
-        sample_node_idx = sample_node_idx.split(logits.shape[1], dim=0)
+        # each mask has shape (n_nodes, hid_dims)
+        sample_node_idx = torch.tensor_split(sample_node_idx, data.ptr[1:-1])
         graphs = Batch.to_data_list(data)
-        # sample subgraphs, each has k nodes
-        list_subgraphs = [subgraphs_from_index(g, i) for g, i in zip(graphs, sample_node_idx)]
+        list_subgraphs = [subgraphs_from_mask(g, i.T) for g, i in zip(graphs, sample_node_idx)]
         list_subgraphs = list(itertools.chain.from_iterable(list_subgraphs))
 
         # new batch
         batch = Batch.from_data_list(list_subgraphs, None, None)
-        original_graph_mask = torch.cat([torch.full((idx.shape[0],), i)
+        original_graph_mask = torch.cat([torch.full((idx.shape[1],), i)
                                          for i, idx in enumerate(sample_node_idx)], dim=0)
         ptr = torch.cat((torch.tensor([0]),
                          (original_graph_mask[1:] > original_graph_mask[:-1]).nonzero().reshape(-1) + 1,
@@ -91,16 +117,43 @@ def train(sample_k: int,
     return train_losses.item() / len(dataloader.dataset)
 
 
-def make_get_batch_topk(ptr, sample_k):
-    @torch.no_grad()
-    def torch_get_batch_topk(logits):
-        logits = logits.detach()
-        logits = torch.tensor_split(logits, ptr)
-        # each has shape (n_subgraphs, n_nodes)
-        sample_node_idx = [torch.topk(l, k=sample_k, dim=0, sorted=False).indices.T.sort(-1).values
-                           for l in logits]
-        sample_node_idx = torch.cat(sample_node_idx, dim=0)
-        sample_node_idx.requires_grad = False
-        return sample_node_idx
+@torch.no_grad()
+def validation(sample_k: int,
+               dataloader: Union[TorchDataLoader, PyGDataLoader, MYDataLoader],
+               emb_model: Emb_model,
+               model: Train_model,
+               criterion: Loss,
+               device: Union[str, torch.device]) -> Union[torch.Tensor, torch.FloatType, float]:
+    emb_model.eval()
+    model.eval()
+    val_losses = torch.tensor(0., device=device)
 
-    return torch_get_batch_topk
+    for data in dataloader:
+        data = data.to(device)
+        logits = emb_model(data)
+
+        torch_get_batch_topk = make_get_batch_topk(data.ptr[1:-1], sample_k)
+        sample_node_idx = torch_get_batch_topk(logits)
+        # each mask has shape (n_nodes, hid_dims)
+        sample_node_idx = torch.tensor_split(sample_node_idx, data.ptr[1:-1])
+        graphs = Batch.to_data_list(data)
+        list_subgraphs = [subgraphs_from_mask(g, i.T) for g, i in zip(graphs, sample_node_idx)]
+        list_subgraphs = list(itertools.chain.from_iterable(list_subgraphs))
+
+        # new batch
+        batch = Batch.from_data_list(list_subgraphs, None, None)
+        original_graph_mask = torch.cat([torch.full((idx.shape[1],), i)
+                                         for i, idx in enumerate(sample_node_idx)], dim=0)
+        ptr = torch.cat((torch.tensor([0]),
+                         (original_graph_mask[1:] > original_graph_mask[:-1]).nonzero().reshape(-1) + 1,
+                         torch.tensor([len(original_graph_mask)])), dim=0)
+        batch.inter_graph_idx = original_graph_mask
+        batch.ptr = ptr
+        batch.y = batch.y[ptr[:-1]]
+
+        pred = model(batch)
+        loss = criterion(pred, batch.y)
+
+        val_losses += loss * batch.num_graphs
+
+    return val_losses.item() / len(dataloader.dataset)

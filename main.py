b5e736cb5e0f269972bdb1c288a595c282e49c1e
Chendi Qian <1049653012@qq.com>
2022-02-03 23:10:48 +0100

    change index source

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ def get_parse() -> Namespace:
     parser.add_argument('--model', type=str, default='gine')
     parser.add_argument('--dataset', type=str, default='zinc')
     parser.add_argument('--hid_size', type=int, default=256)
-    parser.add_argument('--batch_size', type=int, default=4)
-    parser.add_argument('--epochs', type=int, default=5)
+    parser.add_argument('--batch_size', type=int, default=16)
+    parser.add_argument('--epochs', type=int, default=500)
     parser.add_argument('--sample_k', type=int, default=15, help='top-k nodes, i.e. n_nodes of each subgraph')
     parser.add_argument('--num_subgraphs', type=int, default=3, help='number of subgraphs to sample for a graph')
     parser.add_argument('--data_path', type=str, default='./datasets')
@@ -47,29 +47,29 @@ if __name__ == '__main__':
     writer = SummaryWriter(log_path)
 
     # TODO: use full indices
-    with open(os.path.join(args.data_path, 'indices', 'train_indices.pkl'), 'rb') as handle:
-        train_indices = pickle.load(handle)[:32]
-
-    with open(os.path.join(args.data_path, 'indices', 'test_indices.pkl'), 'rb') as handle:
-        test_indices = pickle.load(handle)[:32]
-
-    with open(os.path.join(args.data_path, 'indices', 'val_indices.pkl'), 'rb') as handle:
-        val_indices = pickle.load(handle)[:32]
-
-    # infile = open("./datasets/indices/test.index.txt", "r")
-    # for line in infile:
-    #     indices_test = line.split(",")
-    #     indices_test = [int(i) for i in indices_test]
+    # with open(os.path.join(args.data_path, 'indices', 'train_indices.pkl'), 'rb') as handle:
+    #     train_indices = pickle.load(handle)[:32]
     #
-    # infile = open("./datasets/indices/val.index.txt", "r")
-    # for line in infile:
-    #     indices_val = line.split(",")
-    #     indices_val = [int(i) for i in indices_val]
+    # with open(os.path.join(args.data_path, 'indices', 'test_indices.pkl'), 'rb') as handle:
+    #     test_indices = pickle.load(handle)[:32]
     #
-    # infile = open("./datasets/indices/train.index.txt", "r")
-    # for line in infile:
-    #     indices_train = line.split(",")
-    #     indices_train = [int(i) for i in indices_train]
+    # with open(os.path.join(args.data_path, 'indices', 'val_indices.pkl'), 'rb') as handle:
+    #     val_indices = pickle.load(handle)[:32]
+
+    infile = open("./datasets/indices/test.index.txt", "r")
+    for line in infile:
+        test_indices = line.split(",")
+        test_indices = [int(i) for i in test_indices]
+
+    infile = open("./datasets/indices/val.index.txt", "r")
+    for line in infile:
+        val_indices = line.split(",")
+        val_indices = [int(i) for i in val_indices]
+
+    infile = open("./datasets/indices/train.index.txt", "r")
+    for line in infile:
+        train_indices = line.split(",")
+        train_indices = [int(i) for i in train_indices]
 
     train_loader = MYDataLoader(dataset[:220011][train_indices], batch_size=args.batch_size, shuffle=False,
                                 n_subgraphs=0)

69f029166f2430b86b655c4e4f415d23f36730ff
Chendi Qian <1049653012@qq.com>
2022-03-14 20:57:36 +0100

    add util function

--- data/data_utils.py
+++ data/data_utils.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
+from typing import Dict, Union, Tuple
 
+import torch
 from torch import Tensor
 from torch import device as TorchDevice
 from torch_geometric.data import Data
@@ -46,6 +48,7 @@ class GraphToUndirected:
     Wrapper of to_undirected:
     https://pytorch-geometric.readthedocs.io/en/latest/modules/utils.html?highlight=undirected#torch_geometric.utils.to_undirected
     """
+
     def __call__(self, graph: Data):
         if graph.edge_attr is not None:
             edge_index, edge_attr = to_undirected(graph.edge_index, graph.edge_attr, graph.num_nodes)
@@ -57,3 +60,27 @@ class GraphToUndirected:
                     edge_attr=edge_attr,
                     y=graph.y,
                     num_nodes=graph.num_nodes)
+
+
+def edgeindex2neighbordict(edge_index: torch.Tensor, num_nodes: int, num_edges: int) \
+        -> Tuple[Dict[int, range], Dict[int, Tuple]]:
+    """
+
+    :param edge_index:
+    :param num_nodes:
+    :param num_edges:
+    :return:
+    """
+    assert edge_index[0][0] == 0 and edge_index[0][-1] == num_nodes - 1
+    mask = (torch.where(edge_index[0, 1:] > edge_index[0, :-1])[0] + 1).cpu().tolist()
+    cols = tuple(edge_index[1].cpu().tolist())
+
+    neighbor_idx_dict = {0: range(mask[0])}
+    neighbor_dict = {0: cols[0: mask[0]]}
+
+    for i in range(len(mask) - 1):
+        neighbor_idx_dict[i + 1] = range(mask[i], mask[i + 1])
+        neighbor_dict[i + 1] = cols[mask[i]: mask[i + 1]]
+    neighbor_idx_dict[num_nodes - 1] = range(mask[-1], num_edges)
+    neighbor_dict[num_nodes - 1] = cols[mask[-1]:]
+    return neighbor_idx_dict, neighbor_dict

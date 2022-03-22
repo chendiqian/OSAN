8d9313227701268f88e1cac24a37888e038319eb
Chendi Qian <1049653012@qq.com>
2022-03-22 11:40:02 +0100

    simplify embedding model

--- models/GCN_embd.py
+++ models/GCN_embd.py
@@ -4,12 +4,15 @@ from torch_geometric.utils import degree
 
 
 class GCNConv(MessagePassing):
-    def __init__(self, edge_features, node_features, emb_dim):
+    def __init__(self, edge_features, node_features, emb_dim, update_edge=False):
         super(GCNConv, self).__init__(aggr='add')
+
+        self.update_edge = update_edge
 
         self.linear = torch.nn.Linear(node_features, emb_dim)
         self.edge_encoder = torch.nn.Linear(edge_features, emb_dim)
-        self.edge_updating = torch.nn.Linear(emb_dim, emb_dim)
+        if update_edge:
+            self.edge_updating = torch.nn.Linear(emb_dim, emb_dim)
 
     def forward(self, x, edge_index, edge_embedding):
         x = self.linear(x)
@@ -25,7 +28,8 @@ class GCNConv(MessagePassing):
         norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
 
         new_x = self.propagate(edge_index, x=x, edge_attr=edge_embedding, norm=norm)
-        new_edge_attr = self.edge_updating(new_x[edge_index[0]] + new_x[edge_index[1]]) + edge_embedding
+        new_edge_attr = self.edge_updating(new_x[edge_index[0]] + new_x[edge_index[1]]) + edge_embedding \
+            if self.update_edge else None
         return new_x, new_edge_attr
 
     def message(self, x_j, edge_attr, norm):
@@ -39,15 +43,15 @@ class NetGCN(torch.nn.Module):
     def __init__(self, input_dim, edge_features, hid_dim, emb_dim):
         super(NetGCN, self).__init__()
 
-        self.conv1 = GCNConv(edge_features, input_dim, hid_dim)
+        self.conv1 = GCNConv(edge_features, input_dim, hid_dim, update_edge=False)
         self.bn1 = torch.nn.BatchNorm1d(hid_dim)
-        self.bn_edge1 = torch.nn.BatchNorm1d(hid_dim)
+        # self.bn_edge1 = torch.nn.BatchNorm1d(hid_dim)
 
-        self.conv2 = GCNConv(hid_dim, hid_dim, hid_dim)
+        self.conv2 = GCNConv(edge_features, hid_dim, hid_dim, update_edge=False)
         self.bn2 = torch.nn.BatchNorm1d(hid_dim)
-        self.bn_edge2 = torch.nn.BatchNorm1d(hid_dim)
+        # self.bn_edge2 = torch.nn.BatchNorm1d(hid_dim)
 
-        self.conv3 = GCNConv(hid_dim, hid_dim, hid_dim)
+        self.conv3 = GCNConv(edge_features, hid_dim, hid_dim, update_edge=True)
         self.bn3 = torch.nn.BatchNorm1d(hid_dim)
         self.bn_edge3 = torch.nn.BatchNorm1d(hid_dim)
 
@@ -57,12 +61,12 @@ class NetGCN(torch.nn.Module):
     def forward(self, data):
         x, edge_attr = data.x, data.edge_attr
 
-        x, edge_attr = self.conv1(x, data.edge_index, edge_attr)
+        x, _ = self.conv1(x, data.edge_index, edge_attr)
         x = self.bn1(torch.relu(x))
-        edge_attr = self.bn_edge1(torch.relu(edge_attr))
-        x, edge_attr = self.conv2(x, data.edge_index, edge_attr)
+        # edge_attr = self.bn_edge1(torch.relu(edge_attr))
+        x, _ = self.conv2(x, data.edge_index, edge_attr)
         x = self.bn2(torch.relu(x))
-        edge_attr = self.bn_edge2(torch.relu(edge_attr))
+        # edge_attr = self.bn_edge2(torch.relu(edge_attr))
         x, edge_attr = self.conv3(x, data.edge_index, edge_attr)
         x = self.lin_node(self.bn3(torch.relu(x)))
         edge_attr = self.lin_edge(self.bn_edge3(torch.relu(edge_attr)))

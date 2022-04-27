de3bcaec6ce9a1f2911273b4edc81dd961e0e939
Chendi Qian <1049653012@qq.com>
2022-04-28 01:09:01 +0200

    add const

--- data/const.py
+++ data/const.py
@@ -2,4 +2,5 @@ DATASET_FEATURE_STAT_DICT = {
     'zinc': {'node': 28, 'edge': 3, 'num_class': 1},  # regression
     'mutag': {'node': 7, 'edge': 4, 'num_class': 1},  # bin classification
     'alchemy': {'node': 6, 'edge': 4, 'num_class': 12},  # regression, but 12 labels
+    'ogbg-molesol': {'node': 9, 'edge': 3, 'num_class': 1},  # regression
 }

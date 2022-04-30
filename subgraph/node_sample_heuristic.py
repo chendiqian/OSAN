02131bdffe20a5ef78462bd8f477db6bbcc184bc
Chendi Qian <1049653012@qq.com>
2022-04-30 20:27:16 +0200

    fix bug

--- subgraph/node_sample_heuristic.py
+++ subgraph/node_sample_heuristic.py
@@ -3,7 +3,7 @@ import numba
 import numpy as np
 
 
-@numba.njit(cache=True)
+# @numba.njit(cache=True)
 def numba_sample_node(weight: np.ndarray, k: int):
     mask = np.zeros(weight.shape, dtype=np.bool_)
     close_set = {-1}
@@ -20,8 +20,11 @@ def numba_sample_node(weight: np.ndarray, k: int):
                         mask[i, row] = True
                         count += 1
                 else:
-                    mask[i, row] = True
-                    count += 1
+                    if mask[i, row]:
+                        pass
+                    else:
+                        mask[i, row] = True
+                        count += 1
                 if count >= k:
                     break
             if count >= k:
@@ -43,7 +46,19 @@ def sample_heuristic(weight: torch.Tensor, k: int):
 
 
 if __name__ == '__main__':
-    np.random.seed(42)
-    weight = np.random.rand(10, 6)
-    k = 9
-    mask = numba_sample_node(weight, k)
+    weight = torch.tensor([[0.0471, 4.0962, -0.2081],
+                           [-0.6776, 3.1570, 1.3461],
+                           [-0.5243, 0.4003, 0.3225],
+                           [3.5561, 0.6059, 0.0305],
+                           [-0.0270, 0.0559, 0.5547],
+                           [0.5782, -0.2840, -0.6519],
+                           [2.0829, -1.4955, -0.2017],
+                           [1.4557, 0.0058, -0.0113],
+                           [-0.2255, -0.9465, 0.1351],
+                           [0.0229, -0.2911, -0.3258],
+                           [2.8296, 0.0436, 0.7478],
+                           [-0.6239, 2.1137, 0.8284],
+                           [-0.9461, -1.1008, 0.0775],
+                           [-0.4388, -0.5300, 0.3143]])
+    k = 10
+    mask = sample_heuristic(weight, k)

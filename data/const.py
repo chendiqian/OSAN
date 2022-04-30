6bea3bf717d70c7ca6db9c885c74a29e173614a9
Chendi Qian <1049653012@qq.com>
2022-04-30 02:52:15 +0200

    add stats

--- data/const.py
+++ data/const.py
@@ -3,4 +3,5 @@ DATASET_FEATURE_STAT_DICT = {
     'mutag': {'node': 7, 'edge': 4, 'num_class': 1},  # bin classification
     'alchemy': {'node': 6, 'edge': 4, 'num_class': 12},  # regression, but 12 labels
     'ogbg-molesol': {'node': 9, 'edge': 3, 'num_class': 1},  # regression
+    'ogbg-molbace': {'node': 9, 'edge': 3, 'num_class': 1},  # bin classification
 }

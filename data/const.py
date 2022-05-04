7885cb3671294ea192aab2d148dbaddc7d9a3b7c
Chendi Qian <1049653012@qq.com>
2022-05-05 00:29:26 +0200

    add stats

--- data/const.py
+++ data/const.py
@@ -7,6 +7,7 @@ DATASET_FEATURE_STAT_DICT = {
     'alchemy': {'node': 6, 'edge': 4, 'num_class': 12},  # regression, but 12 labels
     'ogbg-molesol': {'node': 9, 'edge': 3, 'num_class': 1},  # regression
     'ogbg-molbace': {'node': 9, 'edge': 3, 'num_class': 1},  # bin classification
+    'ogbg-molhiv': {'node': 9, 'edge': 3, 'num_class': 1},  # regression
     'qm9': {'node': 11, 'edge': 5, 'num_class': 12},  # regression, 12 labels
     'exp': {'node': 1, 'edge': 0, 'num_class': 1},  # bin classification
 }
@@ -16,6 +17,7 @@ TASK_TYPE_DICT = {
     'alchemy': 'regression',
     'ogbg-molesol': 'regression',
     'ogbg-molbace': 'rocauc',
+    'ogbg-molhiv': 'rocauc',
     'qm9': 'regression',
     'exp': 'acc',
 }
@@ -25,6 +27,7 @@ CRITERION_DICT = {
     'alchemy': nn.L1Loss(),
     'ogbg-molesol': nn.MSELoss(),
     'ogbg-molbace': nn.BCEWithLogitsLoss(),
+    'ogbg-molhiv': nn.BCEWithLogitsLoss(),
     'qm9': nn.L1Loss(),
     'exp': nn.BCEWithLogitsLoss(),
 }
